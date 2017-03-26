9a297fdfb76ae02f1df808eaa3912cc45eaf490d
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2017-03-26 19:02:41 -0300

    travis heroku

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const HTTPS = require('https');
 const Koa = require('koa');
 const KoaStatic = require('koa-static');
 const KoaMount = require('koa-mount');
@@ -27,10 +26,6 @@ module.exports.start = () => new Promise((resolve, reject) => {
 	// app.use(HealthRoutes.allowedMethods());
 
 	app.listen(Config.port);
-
-	if (process.env.NODE_ENV === 'production') {
-		HTTPS.createServer({}, app.callback()).listen(443);
-	}
 
 	return resolve(app);
 });
